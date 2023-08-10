6e753a1d585887d4f23e114a69a6bc589b0e957d
SyrtcevaDaria <syrtceva566333@gmail.com>
2023-08-10 15:25:53 +0300

    solve first task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let array = []
+    let x = 0;
+    let y = 10;
+    for (let i=0;i<expr.length/10;i++){
+        array.push(expr.slice(x, y))
+        x+=10;
+        y+=10;
+    }
+    let resultStr='';
+    for (let elem of array){
+        let res='';
+        if(elem[0]==='*'){
+            resultStr+=" ";
+            continue;
+        }
+        let ind = elem.indexOf('1');
+        for(let i=ind;i<10;i+=2){
+            let symbStr =  elem.slice(i,i+2);
+            if(symbStr==='10'){
+                res+='.';
+            }
+            else{
+                res+='-';
+            }
+        }
+        resultStr+=MORSE_TABLE[res];
+    }
+    return resultStr; 
 }
 
 module.exports = {
